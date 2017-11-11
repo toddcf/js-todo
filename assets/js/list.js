39f531219ac97cbdc5afede7ce1eab09f0f98ea0
Todd Croak-Falen <toddcf@gmail.com>
2017-11-11 13:06:15 -0800

    Minor adjustments.

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 	var todos = [ "Build your own iPod" ];
-	var input = prompt( "What would you like to do?");
+	var input = prompt( "What would you like to do?" );
 
 	while( input !== "quit" ) {
 		if ( input === "list" ) {
@@ -12,12 +12,14 @@ $( document ).ready(function() {
 		else if ( input == "new" ) {
 			var addTodo = prompt( "Enter your new Todo:");
 			todos.push( addTodo );
+			console.log( "Added " + addTodo + " to list." );
 		}
 		else if ( input === "delete" ) {
 			var removal = prompt( "Which number do you want to delete?" );
 			todos.splice( removal, 1 );
+			console.log( "Number " + removal + " has been deleted." );
 		}
-		input = prompt( "What would you like to do next?");
+		input = prompt( "What would you like to do next?" );
 	}
 
 	console.log( "App has been quit." );
