182c82f4a1f68796042fceb4a5ef78fe1a69866f
Todd Croak-Falen <toddcf@gmail.com>
2017-11-10 22:09:05 -0800

    Added ability to print each array element on its own console line along with its index number.

--- assets/js/list.js
+++ assets/js/list.js
@@ -3,7 +3,9 @@ var input = prompt( "What would you like to do?");
 
 while( input !== "quit" ) {
 	if ( input === "list" ) {
-		console.log( todos );
+		todos.forEach( function( todo, i ) {
+			console.log( i + ": "+ todo );
+		});
 	}
 	else if ( input == "new" ) {
 		var addTodo = prompt( "Enter your new Todo:");
