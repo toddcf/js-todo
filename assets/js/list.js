3eb7756856a1e50ab3774e72355ea97bde6fa943
Todd Croak-Falen <toddcf@gmail.com>
2017-11-11 13:11:41 -0800

    Refactored code into functions.

--- assets/js/list.js
+++ assets/js/list.js
@@ -3,21 +3,33 @@ $( document ).ready(function() {
 	var todos = [ "Build your own iPod" ];
 	var input = prompt( "What would you like to do?" );
 
+	function listTodos() {
+		todos.forEach( function( todo, i ) {
+			console.log( i + ": "+ todo );
+		});
+	}
+
+	function addTodo() {
+		var newTodo = prompt( "Enter your new Todo:");
+		todos.push( newTodo );
+		console.log( "Added " + newTodo + " to list." );
+	}
+
+	function deleteTodo() {
+		var removal = prompt( "Which number do you want to delete?" );
+		todos.splice( removal, 1 );
+		console.log( "Number " + removal + " has been deleted." );
+	}
+
 	while( input !== "quit" ) {
 		if ( input === "list" ) {
-			todos.forEach( function( todo, i ) {
-				console.log( i + ": "+ todo );
-			});
+			listTodos();
 		}
 		else if ( input == "new" ) {
-			var addTodo = prompt( "Enter your new Todo:");
-			todos.push( addTodo );
-			console.log( "Added " + addTodo + " to list." );
+			addTodo();
 		}
 		else if ( input === "delete" ) {
-			var removal = prompt( "Which number do you want to delete?" );
-			todos.splice( removal, 1 );
-			console.log( "Number " + removal + " has been deleted." );
+			deleteTodo();
 		}
 		input = prompt( "What would you like to do next?" );
 	}
