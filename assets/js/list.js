361defd386a216525b078996be4d587548eed8c0
Todd Croak-Falen <toddcf@gmail.com>
2017-11-11 12:52:42 -0800

    Added jQuery to prevent js from loading before HTML.

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,17 +1,25 @@
-var todos = [ "Build your own iPod" ];
-var input = prompt( "What would you like to do?");
+$( document ).ready(function() {
 
-while( input !== "quit" ) {
-	if ( input === "list" ) {
-		todos.forEach( function( todo, i ) {
-			console.log( i + ": "+ todo );
-		});
-	}
-	else if ( input == "new" ) {
-		var addTodo = prompt( "Enter your new Todo:");
-		todos.push( addTodo );
-	}
-	input = prompt( "What would you like to do?");
-}
+	var todos = [ "Build your own iPod" ];
+	var input = prompt( "What would you like to do?");
 
-console.log( "App has been quit." );
+	while( input !== "quit" ) {
+		if ( input === "list" ) {
+			todos.forEach( function( todo, i ) {
+				console.log( i + ": "+ todo );
+			});
+		}
+		else if ( input == "new" ) {
+			var addTodo = prompt( "Enter your new Todo:");
+			todos.push( addTodo );
+		}
+		else if ( input === "delete" ) {
+			var removal = prompt( "Which number do you want to delete?" );
+			todos.splice( removal, 1 );
+		}
+		input = prompt( "What would you like to do next?");
+	}
+
+	console.log( "App has been quit." );
+
+});
